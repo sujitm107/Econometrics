b327b87eaced0f33bfe15408d7daf54ac445b617
Sujit <sujit.mollet@rutgers.edu>
2020-04-30 16:15:55 -0400

    changed regression

--- HW2.R
+++ HW2.R
@@ -1,3 +1,5 @@
+# Sujit Molleti and Amith Nitin
+
 data <- read.csv("NJHealth1000.csv", head=T, sep=",")
 summary(data)
 
@@ -6,6 +8,7 @@ str(data) #summarize dataset (look at this along with codebook)
 summary(data) # summary of all the variables
 names(data)  # what variables are included  (look at this along with codebook)
 
+#1st regression
 #declaring new variables
 data$agesq = data$age * data$age
 data$sex.f = factor(data$sex)
@@ -13,8 +16,8 @@ data$sex.f = factor(data$sex)
 #turning off scientific notation
 options(scipen  = 999)
 
-#1st regression
-reg<-lm(weight ~ agesq + sex.f + middleincome + highincome + married + height, data=data)
+
+reg<-lm(weight ~ age + agesq + sex.f + middleincome + highincome + married + height, data=data)
 summary(reg)
 
 library(zoo)
@@ -25,18 +28,44 @@ library(sandwich)
 coeftest(reg, vcov=vcovHC(reg))
 summary(reg)
 
-#adding education and weight
-reg2<-lm(weight ~ agesq + sex.f + middleincome + highincome + married + height + educ + race, data=data)
+#2nd Regression
+#adding education and race
+
+data$race.c = factor(data$race)
+data$educ.c = factor(data$educ)
+
+reg2<-lm(weight ~ age + agesq + sex.f + middleincome + highincome + married + height + educ.c + race.c, data=data)
 coeftest(reg2, vcov=vcovHC(reg2))
 summary(reg2)
 
+#3rd Regression
 #without height
-reg3<-lm(weight ~ agesq + sex.f + middleincome + highincome + married + educ + race, data=data)
+reg3<-lm(weight ~ age + agesq + sex.f + middleincome + highincome + married + educ.c + race.c, data=data)
 coeftest(reg3, vcov=vcovHC(reg3))
 summary(reg3)
 
 #3e
-reg3e<-lm(weight ~ agesq + sex.f + lowincome + middleincome + married + height, data=data)
+reg3e<-lm(weight ~ age + agesq + sex.f + lowincome + middleincome + married + height, data=data)
 summary(reg3e)
 coeftest(reg3e, vcov=vcovHC(reg3e))
+
+
+### Practicing Joint Hypothesis Tests in R ###
+# load built-in data mtcars
+
+#run a regression of mpg on wt (weight), hp (horsepower), and cyl (# cylinders)
+model<-lm(weight ~ agesq + sex.f + middleincome + highincome + married + height + educ + race, data=data)
+
+#model<-lm(mpg~wt+hp+cyl, data=mtcars)
+
+#now we want to test some joint hypotheses. Need package "car" 
+# in order to use linearHypothesis command
+#install.packages("car")
+library(car)
+
+# test joint hypothesis that coef on weight AND coef on cyl both =0
+linearHypothesis(model, c("middleincome=0", "highincome=0"))
+
+# test joint hypothesis that coef on hp = coef on wt
+linearHypothesis(model,c("middleincome=highincome"))
 
